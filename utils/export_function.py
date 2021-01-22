70b800a1a9c95cb854f4f9cbeeb6405380bb7f68
Jing Wei <jingwei.chew@gmail.com>
2021-01-22 19:00:29 +0800

    fix exporter in event of DataParallel

--- utils/export_function.py
+++ utils/export_function.py
@@ -1,7 +1,9 @@
 import os
+from collections import OrderedDict
 
 import torch
 from model import Net
+from torch import nn
 
 def export_model_to_onnx(model, bin_op, output_model_name):
     sample_input = torch.rand((1, 3, 224, 224))
@@ -10,7 +12,15 @@ def export_model_to_onnx(model, bin_op, output_model_name):
     bin_op.binarization()
     torch.save(model.state_dict(), 'temp.pth')
     temp_model = Net()
-    temp_model.load_state_dict(torch.load('temp.pth', map_location='cpu'))
+    # fix this issue: https://discuss.pytorch.org/t/solved-keyerror-unexpected-key-module-encoder-embedding-weight-in-state-dict/1686/3
+    original_state_dict = torch.load('temp.pth', map_location='cpu')
+    if isinstance(model, nn.DataParallel):
+        new_state_dict = OrderedDict()
+        for k, v in original_state_dict.items():
+            name = k[7:] # remove `module.`
+            new_state_dict[name] = v
+        original_state_dict = new_state_dict
+    temp_model.load_state_dict(original_state_dict)
     temp_model.eval()
 
     # Export the model
