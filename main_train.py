553045a10700cd55cf39c55b2f8bff059d1e144c
chew jing wei fyp pdcl <jchew029@scsegc.cm.cluster>
2021-01-31 21:08:07 +0800

    make num_workers a parameter

--- main_train.py
+++ main_train.py
@@ -81,7 +81,7 @@ if __name__ == '__main__':
     trainset = torch.utils.data.Subset(
         trainset, range(args['batch_size'] * 50))
 
-  num_workers=max(torch.cuda.device_count() * 8, 4)
+  num_workers=args['num_workers']
   trainloader = torch.utils.data.DataLoader(trainset,
     batch_size=args['batch_size'],
     shuffle=True,
